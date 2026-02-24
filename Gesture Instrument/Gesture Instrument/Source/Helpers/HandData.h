8148066b551f503815863620334b7c44fde71bb9
Jack <153766980+jackkyle0@users.noreply.github.com>
2026-02-24 21:32:58 +0000

    HandData
    - Removed unused variables

--- Gesture Instrument/Gesture Instrument/Source/Helpers/HandData.h
+++ Gesture Instrument/Gesture Instrument/Source/Helpers/HandData.h
@@ -1,22 +1,15 @@
 #pragma once
 
-struct FingerData
-{
+struct FingerData {
     int type = 0;
-    float tipX = 0.0f, tipY = 0.0f, tipZ = 0.0f;           
-    float joint1X = 0.0f, joint1Y = 0.0f, joint1Z = 0.0f; 
-    float joint2X = 0.0f, joint2Y = 0.0f, joint2Z = 0.0f; 
-    float knuckleX = 0.0f, knuckleY = 0.0f, knuckleZ = 0.0f; 
-
-    // I might not need these ***
-    float fingerPositionX = 0.0f;
-    float fingerPositionY = 0.0f;
-    float fingerPositionZ = 0.0f;
+    float tipX = 0.0f, tipY = 0.0f, tipZ = 0.0f;
+    float joint1X = 0.0f, joint1Y = 0.0f, joint1Z = 0.0f;
+    float joint2X = 0.0f, joint2Y = 0.0f, joint2Z = 0.0f;
+    float knuckleX = 0.0f, knuckleY = 0.0f, knuckleZ = 0.0f;
     bool isExtended = false;
 };
 
-struct HandData
-{
+struct HandData {
     float currentHandPositionX = 0.0f;
     float currentHandPositionY = 0.0f;
     float currentHandPositionZ = 0.0f;
