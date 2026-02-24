896dfb3cdebee7581b857c77ad074f50dd4ea7dc
Jack <153766980+jackkyle0@users.noreply.github.com>
2026-02-24 21:42:03 +0000

    GUI Components
    - Cleanup, fixed braces, removed comments etc

--- Gesture Instrument/Gesture Instrument/Source/UI/GuiComponents.h
+++ Gesture Instrument/Gesture Instrument/Source/UI/GuiComponents.h
@@ -1,68 +1,52 @@
 #pragma once
 #include <JuceHeader.h>
 
-struct MappingRow : public juce::Component
-{
+struct MappingRow : public juce::Component {
     juce::Label label;
     juce::ComboBox comboBox;
 
-    MappingRow(juce::String labelText, int defaultId)
-    {
-        // setup label
+    MappingRow(juce::String labelText, int defaultId) {
         addAndMakeVisible(label);
         label.setText(labelText, juce::dontSendNotification);
         label.setColour(juce::Label::textColourId, juce::Colours::white);
 
-        // setup comboBox
         addAndMakeVisible(comboBox);
 
-        // Options
-        comboBox.addItem("None", 99); 
+        comboBox.addItem("None", 99);
         comboBox.addSeparator();
 
-        // Essentials
-        comboBox.addItem("Volume (CC 7)", 1);
-        comboBox.addItem("Pitch (Bend)", 2);
-        comboBox.addItem("Note Trigger (No Bend)", 13);
-        comboBox.addItem("Modulation (CC 1)", 3);
-        comboBox.addItem("Expression (CC 11)", 4);
+        comboBox.addItem("Volume", 1);
+        comboBox.addItem("Pitch", 2);
+        comboBox.addItem("Note Off", 3);
+        comboBox.addItem("Modulation", 4);
+        comboBox.addItem("Expression", 5);
+        comboBox.addItem("Breath", 6);
+        comboBox.addItem("Cutoff", 7);
+        comboBox.addItem("Resonance", 8);
+        comboBox.addItem("Attack", 9);
+        comboBox.addItem("Release", 10);
+        comboBox.addItem("Vibrato", 11);
+        comboBox.addItem("Pan", 12);
+        comboBox.addItem("Reverb", 13);
+        comboBox.addItem("Chorus", 14);
+        comboBox.addItem("Sustain", 15);
+        comboBox.addItem("Portamento", 16);
 
-        // Shaping
-        comboBox.addItem("Cutoff (CC 74)", 5);
-        comboBox.addItem("Resonance (CC 71)", 6);
-        comboBox.addItem("Vibrato (CC 76)", 10);
-
-        // Spatial
-        comboBox.addItem("Pan (CC 10)", 7);
-        comboBox.addItem("Reverb (CC 91)", 8);
-
-        // Switches
-        comboBox.addItem("Sustain (CC 64)", 9);
-
-        // Macros
-        comboBox.addSeparator();
-        comboBox.addItem("Macro 1 (CC 20)", 11);
-        comboBox.addItem("Macro 2 (CC 21)", 12);
-
-        // Set Default
         comboBox.setSelectedId(defaultId, juce::dontSendNotification);
     }
 
-    void resized() override
-    {
+    void resized() override {
         auto bounds = getLocalBounds();
-        label.setBounds(bounds.removeFromLeft(100)); 
-        comboBox.setBounds(bounds.reduced(0, 2));  
+        label.setBounds(bounds.removeFromLeft(100));
+        comboBox.setBounds(bounds.reduced(0, 2));
     }
 };
 
-struct LabeledSlider : public juce::Component
-{
+struct LabeledSlider : public juce::Component {
     juce::Label label;
     juce::Slider slider;
 
-    LabeledSlider(juce::String name, float min, float max, float initial)
-    {
+    LabeledSlider(juce::String name, float min, float max, float initial) {
         addAndMakeVisible(label);
         label.setText(name, juce::dontSendNotification);
 
@@ -72,8 +56,7 @@ struct LabeledSlider : public juce::Component
         slider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 50, 20);
     }
 
-    void resized() override
-    {
+    void resized() override {
         auto bounds = getLocalBounds();
         label.setBounds(bounds.removeFromLeft(80));
         slider.setBounds(bounds);
