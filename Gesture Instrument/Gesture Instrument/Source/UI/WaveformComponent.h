e5f80a2a15c9119424b7fe13adc7d16374d48ce9
Jack <153766980+jackkyle0@users.noreply.github.com>
2026-03-25 21:03:34 +0000

    Waveform class placeholder

---
+++ Gesture Instrument/Gesture Instrument/Source/UI/WaveformComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <JuceHeader.h>
+
+class WaveformComponent : public juce::Component {
+public:
+    WaveformComponent()
+    {
+       
+    }
+
+    
+    void paint(juce::Graphics& g) override {
+        
+    }
+
+private:
+   
+};
